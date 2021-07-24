176b85a6d93e609a5bdbc570c4e8816f34419243
ryriecshaa <76834995+ryriecshaa@users.noreply.github.com>
2021-07-24 22:24:13 +0700

    Delete Singleton directory

--- Singleton/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args){
-        DataManager account = DataManager.getInstance("197272");
-        DataManager NextAccount = DataManager.getInstance("199999");
-        
-    }
-}
