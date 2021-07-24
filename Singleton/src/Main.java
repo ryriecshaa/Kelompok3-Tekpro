f54bb3c0bd48deb43da3c03966327bb41092f017
ryriecshaa <76834995+ryriecshaa@users.noreply.github.com>
2021-07-24 21:25:24 +0700

    Add files via upload

---
+++ Singleton/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args){
+        DataManager account = DataManager.getInstance("197272");
+        DataManager NextAccount = DataManager.getInstance("199999");
+        
+    }
+}
